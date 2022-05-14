251d6868a5a1f4a89205ae87f4672154162a0df4
HamzaJust <102277205+HamzaJust@users.noreply.github.com>
2022-05-14 22:09:06 +0300

    Add files via upload
    add login form and navigation form

---
+++ Restaurant_ad0027/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Restaurant_ad0027
+{
+    public partial class frmLogin : Form
+    {
+        public frmLogin()
+        {
+            InitializeComponent();
+        }
+
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+            if (txtPassword.Text == "1234")
+            {
+                frmWelcome frm = new frmWelcome(txtUsername.Text);
+                frm.Show();
+                this.Hide();
+            }
+            else
+                MessageBox.Show("Wrong Password!!");
+                
+        }
+
+        
+    }
+}
